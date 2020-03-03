88f7050de5409a71fd030d6bdcde6d46386ce1e1
Advaga <61675633+Advaga@users.noreply.github.com>
2020-03-02 18:17:22 -0600

    Add files via upload

---
+++ Time2.cpp
@@ -0,0 +1,110 @@
+#include "Time2.h"
+
+Time2::Time2(int h, int m, int s, string n) //ctor
+:name(n)//initialize the const members
+{
+    try{
+        setHour(h).setMin(m).setSec(s);
+    }catch(const char * e){
+        throw e;
+    }
+}
+
+Time2::~Time2()
+{
+  //dtor
+}
+
+
+int Time2::getHour() const
+{
+  return hour;
+
+}
+
+int Time2::getMin() const
+{
+  return min;
+}
+
+int Time2::getSec() const
+{
+  return sec;
+}
+
+string Time2::getName() const
+{
+    return name;
+}
+
+
+Time2& Time2::setHour(int h)  
+{
+  if(h <= 23 && h >= 0){
+    hour = h;
+  }else{
+    throw "wrong hour";
+  }
+}
+
+
+Time2& Time2::setMin(int m) 
+{
+  if(m <= 59 && m >= 0){
+    min = m;
+  }else{
+    throw "wrong minute";
+  }
+}
+
+
+Time2& Time2::setSec(int s) 
+{
+  if(s <= 59 && s >= 0){
+    sec = s;
+  }else{
+    throw "wrong second";
+  }
+}
+
+Time2& Time2::operator ++()
+{
+  if(min == 59 && sec ==59){
+    sec = 0;
+    min = 0;
+    hour++;
+  }else if(sec == 59){
+    sec = 0;
+    min++;
+  }else{
+    sec++;
+  } 
+  return *this;
+} 
+
+Time2 Time2::operator ++(int)
+{
+  Time2 temp = *this;
+  ++(*this);
+  return temp; 
+}
+
+Time2& Time2::printTime()
+{
+  cout << "hour" << "-" << "min" << "-" << "sec" << endl;
+  return *this;
+}
+
+Time2& Time2::operator -- ()
+{
+  return *this;
+}
+
+bool Time2::am()
+{
+  if(hour >= 12){
+    cout << "pm" << endl;
+  }else{
+    cout << "am" << endl;
+  }
+} 
---
+++ Time2.h
@@ -0,0 +1,41 @@
+#ifndef TIME2_H
+#define TIME2_H
+
+#include <iostream>
+using namespace std;
+
+class Time2
+{
+    public:
+        //Time2(int h, int m, int s)
+        Time2(int = 0, int = 0, int = 0, string = "None");          //default values
+        ~Time2();
+        int getHour() const;
+        int getMin() const;
+        int getSec() const;
+        bool am();
+        string getName() const;
+        Time2& operator ++();
+        Times2 operator ++(int);
+        Time2& printTime();
+        Time2& operator --();
+        Time2& setHour(int);
+        Time2& setMin(int);
+        Time2& setSec(int);
+        Time2& setAll(int, int, int);
+
+
+    protected:
+
+    private:
+        int hour;
+		    int min; 
+		    int sec;
+        //void setHour(int);
+        //void setMinute(int);
+        //void setSecond(int);
+        const string name; // const object for the class
+
+};
+
+#endif // TIME2_H
